65f5ffb384f78806a9d814643f8182a606c00a55
Yoshi <63078117+YoshiiTsuu@users.noreply.github.com>
2021-05-27 16:07:28 -0300

    update

--- src/main/java/br/org/generation/myBlog/controller/PostagemController.java
+++ src/main/java/br/org/generation/myBlog/controller/PostagemController.java
@@ -3,10 +3,15 @@ package br.org.generation.myBlog.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,22 +19,44 @@ import br.org.generation.myBlog.model.Postagem;
 import br.org.generation.myBlog.repository.PostagemRepository;
 
 @RestController
-@RequestMapping("/postagens")
+@RequestMapping("/postagem")
 @CrossOrigin("*")
 public class PostagemController {
 
 	@Autowired
 	private PostagemRepository repository;
 
-	@GetMapping
+	@GetMapping() //Findall
 	public ResponseEntity<List<Postagem>> GetAll() {
 		return ResponseEntity.ok(repository.findAll());
 	}
 
-	@GetMapping("/{id}")
+	@GetMapping("/{id}") //GetById
 	public ResponseEntity<Postagem> GetById(@PathVariable long id) {
-		return repository.findById(id).map(resp -> ResponseEntity.ok(resp))//devolve um bojeto resposta
-				.orElse(ResponseEntity.notFound().build());//caso contrário devolve um notfound
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp))// devolve um objeto resposta
+				.orElse(ResponseEntity.notFound().build());// caso contrário devolve um notfound
+	}
 
+	@GetMapping("/titulo/{titulo}") //GetByTitulo
+	public ResponseEntity<List<Postagem>> GetByTitulo(@PathVariable String titulo) {
+		return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo)); // Este método retorna uma
+																							// lista
+	}
+
+	@GetMapping("/texto/{texto}")
+	public ResponseEntity<List<Postagem>> GetByTexto(@PathVariable String texto) {
+		return ResponseEntity.ok(repository.findAllByTextoContainingIgnoreCase(texto));
+	}
+	@PostMapping
+	public ResponseEntity<Postagem> post (@RequestBody Postagem postagem){
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postagem));
+	}
+	@PutMapping
+	public ResponseEntity<Postagem> put (@RequestBody Postagem update){
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(update));		
+	}
+	@DeleteMapping
+	public void delete(@PathVariable long id) {
+		repository.deleteById(id);
 	}
 }
