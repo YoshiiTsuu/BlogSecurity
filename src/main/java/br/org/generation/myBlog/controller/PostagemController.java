f12a951fbf742ad8673b5f40eb854e4d3a3c2ece
Yoshi <63078117+YoshiiTsuu@users.noreply.github.com>
2021-05-27 00:06:31 -0300

    update

--- src/main/java/br/org/generation/myBlog/controller/PostagemController.java
+++ src/main/java/br/org/generation/myBlog/controller/PostagemController.java
@@ -2,11 +2,11 @@ package br.org.generation.myBlog.controller;
 
 import java.util.List;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,9 +20,16 @@ public class PostagemController {
 
 	@Autowired
 	private PostagemRepository repository;
-	
+
 	@GetMapping
-	public ResponseEntity<List<Postagem>> GetAll(){
+	public ResponseEntity<List<Postagem>> GetAll() {
 		return ResponseEntity.ok(repository.findAll());
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Postagem> GetById(@PathVariable long id) {
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp))//devolve um bojeto resposta
+				.orElse(ResponseEntity.notFound().build());//caso contrário devolve um notfound
+
 	}
 }
